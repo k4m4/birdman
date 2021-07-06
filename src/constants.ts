cec15e7b97a306affd6523332c209fcf91ff3833
k4m4 <nikolaskam@gmail.com>
2021-07-06 07:17:54 -0400

    Update node IP & port

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,7 @@ export const NODE_AGENT = `Birdman Client ${BIRDMAN_VERSION}`;
 export const KNOWN_PEERS_FILENAME = 'data/peers.txt';
 export const KNOWN_OBJECTS_FILENAME = 'data/objects.txt';
 
-export const PORT = 3000;
-export const IP = '127.0.0.1';
+export const PORT = 1337;
+export const IP = '0.0.0.0';
 
 export const BIRDMAN_ADDRESS = '146.185.182.146:1337';
